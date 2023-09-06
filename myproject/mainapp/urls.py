cc8665d405905c94f9c6aecf4bc1f2c0570cf2d2
Anton <Afrser@gmail.com>
2023-09-06 23:24:29 +0300

    HW_3

--- myproject/mainapp/urls.py
+++ myproject/mainapp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
-
+from .views import Home, About, Contact, Orders
 
 urlpatterns = [
- path('', views.index, name='index'),
- path('about/', views.about, name='about'),
+    path('', Home.as_view(), name='index'),
+    path('index', Home.as_view(), name='index'),
+    path('about/', About.as_view(), name='about'),
+    path('contact/', Contact.as_view(), name='contact'),
+    path('orders/', Orders.as_view(), name='orders'),
 ]
