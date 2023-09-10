13f63afb0d67a0cfb20dfc4a8cf3da6bee145392
Anton <Afrser@gmail.com>
2023-09-10 16:35:44 +0300

    HW_4

--- myproject/mainapp/urls.py
+++ myproject/mainapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
-from .views import Home, About, Contact, Orders
+from .views import Home, About, Contact, Orders, ProductCreate
 
 urlpatterns = [
     path('', Home.as_view(), name='index'),
-    path('index', Home.as_view(), name='index'),
+    path('index/', Home.as_view(), name='index'),
     path('about/', About.as_view(), name='about'),
     path('contact/', Contact.as_view(), name='contact'),
     path('orders/', Orders.as_view(), name='orders'),
+    path('product_create/', ProductCreate.as_view(), name='product_create'),
 ]
--- myproject/mainapp/views.py
+++ myproject/mainapp/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
-from django.views.generic import ListView, TemplateView
+from django.views.generic import ListView, TemplateView, CreateView
 
-from mainapp.models import Order
+from mainapp.forms import ProductForm
+from mainapp.models import Order, Product
 
 MAIN_MENU = [
-            {'href': 'index', 'name': 'Главная'},
-            {'href': 'about', 'name': 'О компании'},
-            {'href': 'orders', 'name': 'Заказы'},
-            {'href': 'contact', 'name': 'Контакты'},
-        ]
+    {'href': 'index', 'name': 'Главная'},
+    {'href': 'about', 'name': 'О компании'},
+    {'href': 'orders', 'name': 'Заказы'},
+    {'href': 'contact', 'name': 'Контакты'},
+    {'href': 'product_create', 'name': 'Добавить фото продукта'},
+]
+
 
 class Home(TemplateView):
     template_name = 'mainapp\index.html'
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['main_menu'] = MAIN_MENU
@@ -41,9 +45,24 @@ class Contact(TemplateView):
 class Orders(ListView):
     template_name = 'mainapp/orders.html'
     model = Order
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['main_menu'] = MAIN_MENU
         context['orders'] = Order.objects.all()
         return context
 
+
+class ProductCreate(CreateView):
+    # Модель куда выполняется сохранение
+    model = Product
+    # Класс на основе которого будет валидация полей
+    form_class = ProductForm
+    # Выведем все существующие записи на странице
+    extra_context = {'products': Product.objects.all()}
+    # Шаблон с помощью которого
+    # будут выводиться данные
+    template_name = 'mainapp/product_create.html'
+    # На какую страницу будет перенаправление
+    # в случае успешного сохранения формы
+    success_url = '/index'
